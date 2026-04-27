63fed6bf56b83cca910eb0200ea7f58d2068e5c1
Thomas Klinger <thomas.klinger@otto.de>
2026-04-27 18:21:49 +0200

    feat: initial baseline for vfp
    this patch sketches the anticipated strategy for the vmod:
    
    - a VFP does the (de)compression
    - the VFP is registered in vmod_event
    
    the VFP interface is satisfied for now but cannot be used as it does
    nothing at the moment. a future patch needs to correctly initialize
    libzstd and consume the byte stream.

--- src/vmod_zstd.c
+++ src/vmod_zstd.c
@@ -14,8 +14,45 @@
 #include <zstd.h>
 
 #define VZSTD_FAIL(ctx, fmt, ...) VRT_fail((ctx), "ZSTD: " fmt, __VA_ARGS__)
-#define VZSTD_LOGf(tag, ctx, fmt, ...) VSL((tag), ((ctx) == NULL || (ctx)->sp == NULL) ? NO_VXID : (ctx)->sp->vxid, "ZSTD: " fmt, __VA_ARGS__)
-#define VZSTD_LOG(tag, ctx, fmt) VZSTD_LOGf((tag), (ctx), "%s", fmt)
+#define VZSTD_LOGf(tag, vxid, fmt, ...) VSL((tag), (vxid), "ZSTD: " fmt, __VA_ARGS__)
+#define VZSTD_LOG(tag, vxid, fmt) VZSTD_LOGf((tag), (vxid), "%s", fmt)
+
+static enum vfp_status v_matchproto_(vfp_init_f)
+vfp_zstd_init(VRT_CTX, struct vfp_ctx *vc, struct vfp_entry *vfe) {
+	CHECK_OBJ_NOTNULL(vc, VFP_CTX_MAGIC);
+	CHECK_OBJ_NOTNULL(vfe, VFP_ENTRY_MAGIC);
+	AN(vfe->vfp);
+
+	VZSTD_LOG(SLT_Notice, NO_VXID, "initializing vfp_zstd");
+
+	return (VFP_OK);
+}
+
+static void v_matchproto_(vfp_fini_f)
+vfp_zstd_fini(struct vfp_ctx *vc, struct vfp_entry *vfe) {
+	CHECK_OBJ_NOTNULL(vc, VFP_CTX_MAGIC);
+	CHECK_OBJ_NOTNULL(vfe, VFP_ENTRY_MAGIC);
+	AN(vfe->vfp);
+
+	VZSTD_LOG(SLT_Notice, NO_VXID, "destroying vfp_zstd");
+}
+
+static enum vfp_status v_matchproto_(vfp_pull_f)
+vfp_zstd_pull(struct vfp_ctx *vc, struct vfp_entry *vfe, void *ptr, ssize_t *lenp) {
+	CHECK_OBJ_NOTNULL(vc, VFP_CTX_MAGIC);
+	CHECK_OBJ_NOTNULL(vfe, VFP_ENTRY_MAGIC);
+	AN(vfe->vfp);
+
+    return (VFP_END);
+}
+
+static const struct vfp vfp_zstd = {
+	.name = "zstd",
+	.init = vfp_zstd_init,
+	.fini = vfp_zstd_fini,
+	.pull = vfp_zstd_pull,
+	.priv1 = NULL,
+};
 
 VCL_STRING
 vmod_version(VRT_CTX) {
@@ -31,10 +68,20 @@ vmod_event(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
     CHECK_OBJ_NOTNULL(ctx, VRT_CTX_MAGIC);
     AN(priv);
 
+    const char *err;
+
     switch(e) {
     case VCL_EVENT_LOAD:
+        VZSTD_LOGf(SLT_Debug, NO_VXID, "adding vfp_zstd as [%s]", vfp_zstd.name);
+        err = VRT_AddFilter(ctx, &vfp_zstd, NULL);
+        if (err != NULL) {
+            VZSTD_LOGf(SLT_Error, NO_VXID, "adding vfp_zstd [%s] failed with error [%s]", vfp_zstd.name, err);
+            return(1);
+        }
         return (0);
     case VCL_EVENT_DISCARD:
+        VZSTD_LOGf(SLT_Debug, NO_VXID, "removing vfp_zstd [%s]", vfp_zstd.name);
+        VRT_RemoveFilter(ctx, &vfp_zstd, NULL);
         return (0);
     case VCL_EVENT_WARM:
         return (0);
